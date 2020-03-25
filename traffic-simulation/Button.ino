7b4b093cbe3e6b62cb0662c13ed78f25ac94e329
Rowan-Paul <46132597+Rowan-Paul@users.noreply.github.com>
2020-03-25 09:47:59 +0100

    Fixed second button
    Changed:
     - buttonSetup & buttonPressed to work with a variable allowing multiple buttons
    
    Added:
     - (temporary) test to stateMachine to check lights/buttons

--- traffic-simulation/Button.ino
+++ traffic-simulation/Button.ino
@@ -1,17 +1,13 @@
-const int BUTTON_PIN = 4;
-
-void buttonSetup() {
+void buttonSetup(const int BUTTON_PIN) {
   pinMode(BUTTON_PIN, INPUT);
 }
 
-boolean buttonPressed() {
+boolean buttonPressed(const int BUTTON_PIN) {
   int buttonState = digitalRead(BUTTON_PIN);
   // Is button pressed?
   if (buttonState == LOW) {
-    Serial.println("Aan");
     return true;
   } else {
-    Serial.println("Uit");
     return false;
   }
 }
--- traffic-simulation/StateMachine.ino
+++ traffic-simulation/StateMachine.ino
@@ -1,11 +1,24 @@
 void stateMachineSetup() {
-  buttonSetup();
+  buttonSetup(4);
+  buttonSetup(8);
   ledControlSetup();
 }
 
 void stateMachineLoop() {
-  if (buttonPressed(4 /* or 8 */)) {
+  if (buttonPressed(4)) {
     // check if all other lights are red and change light to green
-    Serial.println("Knop ingedrukt");
+    Serial.println("Button 1 presssed");
+    ledControlSetLedOn(5);
+    ledControlSetLedOff(7);
+  } else if (buttonPressed(8)) {
+    // check if all other lights are red and change light to green
+    Serial.println("Button 2 pressed");
+    ledControlSetLedOn(9);
+    ledControlSetLedOff(11);
+  } else {
+    ledControlSetLedOff(5);
+    ledControlSetLedOn(7);
+    ledControlSetLedOff(9);
+    ledControlSetLedOn(11);
   }
 }
